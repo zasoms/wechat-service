1f0d5767d589e2842da0069aecd0872fd8cafd7d
zasoms <623064100@qq.com>
2021-10-16 00:11:06 +0800

    fix: 发送信息无成功f返回

--- controllers/message.js
+++ controllers/message.js
@@ -14,8 +14,18 @@ exports.sendget = async (ctx) => {
       message: '请传入发送的内容'
     }
   }
-
-  pusher(query.topic, { type: 'text', content: query.content })
+  try {
+    pusher(query.topic, { type: 'text', content: query.content })
+    ctx.body = {
+      code: 0,
+      message: '发送成功'
+    }
+  } catch(e) {
+    ctx.body = {
+      code: -1,
+      message: e.toString()
+    }
+  }
 }
 exports.sendpost = async (ctx) => {
   ctx.body = {
